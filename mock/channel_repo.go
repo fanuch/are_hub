15364c9239f7c1e9141970e1ccc7d077aedf3b67
blacksfk <blacksunflowakuttaspam@hotmail.com>
2021-03-18 01:48:32 +1100

    now actually implements ChannelRepo. forgot pointer receivers

--- mock/channel_repo.go
+++ mock/channel_repo.go
@@ -27,32 +27,36 @@ type ChannelRepo struct {
 	CountCalled bool
 }
 
-func (r ChannelRepo) All(ctx context.Context) ([]are_server.Channel, error) {
+func (r *ChannelRepo) All(ctx context.Context) ([]are_server.Channel, error) {
 	r.AllCalled = true
 
 	return r.AllFunc(ctx)
 }
 
-func (r ChannelRepo) Insert(ctx context.Context, archetype are_server.Archetype) error {
+func (r *ChannelRepo) Insert(ctx context.Context, archetype are_server.Archetype) error {
 	r.InsertCalled = true
 
 	return r.InsertFunc(ctx, archetype)
 }
 
-func (r ChannelRepo) FindID(ctx context.Context, id string) (*are_server.Channel, error) {
+func (r *ChannelRepo) FindID(ctx context.Context, id string) (*are_server.Channel, error) {
 	r.FindIDCalled = true
 
 	return r.FindIDFunc(ctx, id)
 }
 
-func (r ChannelRepo) UpdateID(ctx context.Context, id string, channel *are_server.Channel) error {
+func (r *ChannelRepo) UpdateID(ctx context.Context, id string, channel *are_server.Channel) error {
 	r.UpdateIDCalled = true
 
 	return r.UpdateIDFunc(ctx, id, channel)
 }
 
-func (r ChannelRepo) DeleteID(ctx context.Context, id string) (*are_server.Channel, error) {
+func (r *ChannelRepo) DeleteID(ctx context.Context, id string) (*are_server.Channel, error) {
 	r.DeleteIDCalled = true
 
 	return r.DeleteIDFunc(ctx, id)
+}
+
+func (r *ChannelRepo) Count(ctx context.Context) (int64, error) {
+	return 0, nil
 }
