39b662d44d30857b533ac9937f59e62c40dd1453
blacksfk <blacksunflowakuttaspam@hotmail.com>
2021-03-18 02:15:48 +1100

    created context keys and ContextEmbeddable interface

---
+++ context.go
@@ -0,0 +1,18 @@
+package are_server
+
+import "context"
+
+// Unique, unexported key type to prevent collisions
+type ctxKey uint
+
+// See: https://golang.org/ref/spec#Constant_declarations
+// and: https://golang.org/ref/spec#Iota
+// and: https://golang.org/doc/effective_go.html#constants
+const (
+	keyChannel ctxKey = iota
+)
+
+// Types implementing this interface can be stored in a context.
+type ContextEmbeddable interface {
+	ToCtx(context.Context) context.Context
+}
