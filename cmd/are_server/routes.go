17c487621fbada126fdd022801b441383ef57cba
blacksfk <blacksunflowakuttaspam@hotmail.com>
2021-03-19 20:16:37 +1100

    channel route definitions

---
+++ cmd/are_server/routes.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/blacksfk/are_server/http"
+	"github.com/blacksfk/are_server/http/middleware/validate"
+	uf "github.com/blacksfk/microframework"
+)
+
+// HTTP route definitions.
+func routes(s *uf.Server, services *services) {
+	// channel routes
+	c := http.NewChannel(services.channels)
+	v := validate.NewChannel()
+
+	s.NewGroup("/channel").Get(c.Index).Post(c.Store, v.ChannelStore)
+	s.NewGroup("/channel/:id").Get(c.Show).Put(c.Update, v.ChannelStore).Delete(c.Delete)
+}
