c9eda5b0a471d09b6f0099434b0fea3ab8badffc
blacksfk <blacksunflowakuttaspam@hotmail.com>
2021-03-25 14:18:23 +1100

    added ws routes

--- cmd/are_server/routes.go
+++ cmd/are_server/routes.go
@@ -4,6 +4,7 @@ import (
 	"github.com/blacksfk/are_server/http"
 	"github.com/blacksfk/are_server/http/middleware/validate"
 	uf "github.com/blacksfk/microframework"
+	"nhooyr.io/websocket"
 )
 
 // HTTP route definitions.
@@ -14,4 +15,15 @@ func routes(s *uf.Server, services *services) {
 
 	s.NewGroup("/channel").Get(c.Index).Post(c.Store, v.Store)
 	s.NewGroup("/channel/:id").Get(c.Show).Put(c.Update, v.Store).Delete(c.Delete)
+
+	// websocket upgrade routes
+	opts := &websocket.AcceptOptions{
+		InsecureSkipVerify: true,
+		CompressionMode:    websocket.CompressionDisabled,
+	}
+
+	ts := http.NewTelemetryServer(opts, services.channels)
+
+	s.Get("/subscribe/:id", ts.Subscribe)
+	s.Get("/publish/:id", ts.Publish)
 }
